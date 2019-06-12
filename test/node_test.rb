cfb6bb0df682ca3061d2331ce9e4614646919bdf
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-11 21:49:01 -0400

    Add data argument for init of Node

--- test/node_test.rb
+++ test/node_test.rb
@@ -2,10 +2,14 @@ require './test/test_helper'
 
 class NodeTest < Minitest::Test
   def setup
-    @node = Node.new
+    @node = Node.new("plop")
   end
 
   def test_it_exists
     assert_instance_of Node, @node
+  end
+
+  def test_it_has_data_from_init
+    assert_equal "plop", @node.data
   end
 end
