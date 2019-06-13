31c3032b10eece89ba5aae6b5036da468c1902de
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-12 20:40:40 -0400

    Add JungleBeat#append method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -6,4 +6,12 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
+
+  def append(data)
+    data_elems = data.split
+    data_elems.each do |data_element|
+      @list.append(data_element)
+    end
+    data
+  end
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -13,4 +13,14 @@ class JungleBeatTest < Minitest::Test
     assert_instance_of LinkedList, @jb.list
     assert_nil @jb.list.head
   end
+
+  def test_it_can_append_multiple_nodes_at_once
+    assert_equal "deep doo ditt", @jb.append("deep doo ditt")
+    assert_equal "deep", @jb.list.head.data
+    assert_equal "doo", @jb.list.head.next_node.data
+
+    # assert_equal "woo hoo shu", @jb.append("woo hoo shu")
+    # assert_equal 6, @jb.count
+    # assert_equal "deep doo ditt woo hoo shu", @jb.list.to_string
+  end
 end
